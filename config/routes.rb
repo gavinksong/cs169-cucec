cb68d08913ca56c25e3ce42c6eda38db03a89cee
gavinksong <gavinksong@gmail.com>
2016-10-18 02:25:00 +0000

    Add Devise models: Student and Mentor.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :mentors
+  devise_for :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 end
