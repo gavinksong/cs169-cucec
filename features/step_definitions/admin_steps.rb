ed20839c6ffc790a09b30a9a3367fe785bf1596b
Yadel <yabraham@berkely.edu>
2016-10-26 23:53:43 -0700

    Added Cucumber features for admin nav page.

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,3 +1,4 @@
+
 Given /^I am logged in as an admin$/ do
   step 'an admin with email "email@gmail.com" and password "mypassword" exists'
   visit '/admins'
@@ -7,4 +8,8 @@ Given /^I am logged in as an admin$/ do
   if page.respond_to? :should
     step 'I should be on the admin dashboard page'
   end
+=======
+When(/^I upload a file$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+
 end
