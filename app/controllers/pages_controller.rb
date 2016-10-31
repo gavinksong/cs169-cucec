f815febd3039e0989456aedac00a88f529f60751
Kseniia Ryuma <kryuma@berkeley.edu>
2016-10-31 00:44:38 +0000

    admin start

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,10 @@
 class PagesController < ApplicationController
+    
+    # def index
+   
+    #     if current_mentor.admin?
+    #         redirect_to admin_index_path
+    #     end
+    # end
 end
+
