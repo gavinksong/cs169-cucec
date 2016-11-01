0a493c9c130e26429c989d6a26b9b7ccb7745349
Kseniia Ryuma <kryuma@berkeley.edu>
2016-11-01 04:38:32 +0000

    admin's page update

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,16 @@ Rails.application.routes.draw do
   devise_for :mentors
   devise_for :students
   
+  resources :admin
+  #devise_root
+  authenticated :admin do
+    root :to => 'admins#index'
+  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   
-  resources :admin
+
   
-  #devise_root
-  authenticated :admin do
-    root 'admins#index', as: :authenticated_root
-  end
+
 end
