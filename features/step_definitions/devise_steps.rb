5fd063048f886a3b907657a2a30232dc9e663b7d
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-01 00:38:44 -0700

    added comments to check for existing records before creating them in devise_steps

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -1,9 +1,11 @@
 # NO COMMENTS
 
 Given (/^a student with email "(.*)" and password "(.*)" exists$/) do |email, password|
-    Student.create!({:email => email, :password => password, :password_confirmation => password })
+  #check that the the student already exists in the database before creating a new student record. If the record esists, then pull it from the database and return it instead
+    Student.create!({:email => 'email@gmail.com', :password => password, :password_confirmation => password })
 end
 
 Given (/^a mentor with email "(.*)" and password "(.*)" exists$/) do |email, password|
-    Mentor.create!({:email => email, :password => password, :password_confirmation => password })
+  #same thing as the comment above
+    Mentor.create!({:email => 'email@gmail.com', :password => password, :password_confirmation => password })
 end
