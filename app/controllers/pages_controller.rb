2e05f97de9db1eda46a180652a300e4db7f4b2e3
gavinksong <gavinksong@gmail.com>
2016-11-03 05:21:11 +0000

    Redirect to mentor navigation page on login. Authenticate mentor routes.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  def listen
-    # @audio = Audio.find('') # attempt to retrieve audio from database
-    @audio = ''
-  end
+    before_action :redirect_to_navigation
+    
+    def redirect_to_navigation
+        if current_mentor
+            redirect_to '/mentors/index'
+        end
+    end
+
+    def listen
+        # @audio = Audio.find('') # attempt to retrieve audio from database
+        @audio = ''
+    end
 end
