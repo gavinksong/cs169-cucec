e0682fa38a3c15021dbda60ebea2ec1b5eb69422
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-02 22:46:19 -0700

    removed listen action and removed unnecessary comments in devise action

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -1,11 +1,9 @@
 # NO COMMENTS
 
 Given (/^a student with email "(.*)" and password "(.*)" exists$/) do |email, password|
-  #check that the the student already exists in the database before creating a new student record. If the record esists, then pull it from the database and return it instead
     Student.create!({:email => email, :password => password, :password_confirmation => password })
 end
 
 Given (/^a mentor with email "(.*)" and password "(.*)" exists$/) do |email, password|
-  #same thing as the comment above
     Mentor.create!({:email => 'email@gmail.com', :password => password, :password_confirmation => password })
 end
