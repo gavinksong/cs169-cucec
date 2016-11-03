ac13655d39c4c9135b7dedc2fc68dbaf2cb4f5d9
Bao Mai <baomai@berkeley.edu>
2016-11-02 21:02:22 -0700

    View refactor

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
 
-
+  resource :student do
+    get 'navigation', to: 'students#navigation'
+  end
 end
