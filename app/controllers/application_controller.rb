cd420dd067eba25fb56b0213384c6b57f642aa5e
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-05 16:19:39 -0700

    implemented login redirect to respective dashboards for students and mentors respectively

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def current_user
-    @current_user ||= (warden.authenticate(scope: :student) || nil)
+  def after_sign_in_path_for(resource)
+    if student_signed_in?
+      return students_path
+    end
+    if mentor_signed_in?
+      return mentors_path
+    end
+    #admin redirect to come in here as well
   end
 end
