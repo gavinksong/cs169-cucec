b9855150d30d91111bc2670d5e1a779fc2886a85
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-05 16:39:47 -0700

    added all relevant routes in cucumber paths file

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -13,38 +13,49 @@ module NavigationHelpers
   def path_to(page_name)
     case page_name
 
-    when /^the home\s?page$/
+    when /^the home\s?page$/, /^the landing page$/
       '/'
+      
+    # mentor paths
+    when /^the mentor dashboard page$/
+      mentors_path
     when /^the mentor login page$/
-      new_mentor_session_path        
-    when /^the student login page$/
-      new_student_session_path
+      new_mentor_session_path   
     when /^the mentor signup page$/
       new_mentor_registration_path
+      
+    when /^the mentor call page$/
+      mentors_call_path  
+    when /^the mentor chat page$/
+      mentors_chat_path 
+      
+    # student paths
+    when /^the student dashboard page$/
+      students_path
+    when /^the student login page$/
+      new_student_session_path
     when /^the student signup page$/
       new_student_registration_path
-    when /^the mentor navigation page$/
-      '/mentors/index'
-    when /^the student navigation page$/
-      navigation_students_path
-    when /^the student Listen page$/
-     listen_path
-    when /^the student video page$/ 
-      watch_path
-    when /^the student Speak page$/
-      speaking_students_path
-    when /^the student Read page$/
-      reading_students_path
-    when /^the student Write page$/
-      writing_students_path
-    when /^the student Watch page$/
-      watching_students_path
-    when /^the Chat page$/
+      
+    when /^the student listen page$/
+      students_listen_path
+    when /^the student watch page$/ 
+      students_watch_path
+    when /^the student speak page$/
+      students_speak_path
+    when /^the student read page$/
+      students_read_path
+    when /^the student write page$/
+      students_write_path
+    when /^the student chat page$/
+      students_chat_path
+      
+    when /^the conversation page$/
       conversations_path
       #doing this because there is no need for new chat
       # chat_path
     when /^the donation page$/
-      '/donate/new'
+      new_donate_path
 
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
