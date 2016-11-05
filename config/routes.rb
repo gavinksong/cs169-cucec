d1c2c86be2e12a7311e10d7507da5f6dee2b3b49
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-05 15:48:27 -0700

    changed resourceful routes students and mentors

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,6 @@ Rails.application.routes.draw do
   
   devise_for :mentors
   devise_for :students
-  authenticate(:student) do
-    resource :students do
-      get 'listen' => 'students#listen'
-      get 'speak' => 'students#speak'
-      get 'read' => 'students#read'
-      get 'write' => 'students#write'
-      post 'write' => 'students#createwriting'
-      get 'chat' => 'students#chat'
-      get 'watch' => 'students#watch'
-    end
-  end
   
   resources :conversations do
     resources :messages
@@ -22,7 +11,17 @@ Rails.application.routes.draw do
   
   resources :donate
   
-  resource :mentors do
-  end
-  
+  # students routes
+  get 'students' => 'students#index'
+  get 'students/listen' => 'students#listen'
+  get 'students/speak' => 'students#speak'
+  get 'students/read' => 'students#read'
+  get 'students/write' => 'students#write'
+  get 'students/chat' => 'students#chat'
+  get 'students/watch' => 'students#watch'
+
+  # mentors routes
+  get 'mentors' => 'mentors#index'
+  get 'mentors/call' => 'mentors/chat'
+  get 'mentors/chat' => 'mentors/call'
 end
