c953627314ac020fa01b364efb8721a549793135
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-05 18:21:14 -0700

    all cucumber test pasing - admin hasn't been implemented

--- features/step_definitions/mentors_steps.rb
+++ features/step_definitions/mentors_steps.rb
@@ -1,6 +1,6 @@
 Given /^I am logged in as a mentor$/ do
   step 'a mentor with email "email@gmail.com" and password "mypassword" exists'
-  visit '/mentors/sign_in'
+  visit new_mentor_session_path
   fill_in 'Email', :with => 'email@gmail.com'
   fill_in 'Password', :with => 'mypassword'
   click_button 'Log in'
