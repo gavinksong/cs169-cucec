92e0a8f7457296ba0b4ff92089e8d56e869b7ac5
Emilio Aurea <emilioaurea@gmail.com>
2016-11-08 21:13:24 +0000

    fixed migration

--- db/migrate/20161104232934_create_writings.rb
+++
@@ -1,7 +0,0 @@
-class CreateWritings < ActiveRecord::Migration[5.0]
-  def change
-    create_table :writings do |t|
-      t.text :sample
-    end
-  end
-end
--- db/migrate/20161104233254_add_student_id_to_writings.rb
+++
@@ -1,5 +0,0 @@
-class AddStudentIdToWritings < ActiveRecord::Migration[5.0]
-  def change
-    add_column :writings, :student_id, :integer
-  end
-end
