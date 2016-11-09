91f16d42536a3325702a316e65aeadd1df27336f
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-08 19:00:51 -0800

    deleted admin files

--- features/step_definitions/devise_steps.rb
+++ features/step_definitions/devise_steps.rb
@@ -5,7 +5,3 @@ end
 Given (/^a mentor with email "(.*)" and password "(.*)" exists$/) do |email, password|
     Mentor.create!({:email => email, :password => password, :password_confirmation => password })
 end
-
-Given (/^an admin with email "(.*)" and password "(.*)" exists$/) do |email, password|
-    Admin.create!({:email => email, :password => password, :password_confirmation => password })
-end
