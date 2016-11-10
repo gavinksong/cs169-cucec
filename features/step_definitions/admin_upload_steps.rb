a8f5781da3329135c3019743beb390c140a28edb
Emilio Aurea <emilioaurea@gmail.com>
2016-11-10 05:15:18 +0000

    completed cucumber scenario for uploading text file

---
+++ features/step_definitions/admin_upload_steps.rb
@@ -0,0 +1,19 @@
+When(/^I upload a text file with (valid|no) content and (.*)'s date$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I logout$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^I visit the student read page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see valid content$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see empty content$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
