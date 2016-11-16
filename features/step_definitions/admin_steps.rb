cdc67b2e64d5e6bd48657b9ff8b5229317d04aee
Emilio Aurea <emilioaurea@gmail.com>
2016-11-16 03:41:55 +0000

    Modified/Created feature files for the Admin file upload feature.

--- features/step_definitions/admin_steps.rb
+++ features/step_definitions/admin_steps.rb
@@ -1,12 +1,10 @@
 Given /^I am logged in as an admin$/ do
   step 'an admin with email "email@gmail.com" and password "mypassword" exists'
-  visit '/admin/sign_in'
+  visit '/admins'
   fill_in 'Email', :with => 'email@gmail.com'
   fill_in 'Password', :with => 'mypassword'
   click_button 'Log in'
   if page.respond_to? :should
-    step 'I should be on the mentor dashboard page'
-  else
-    step 'I should be on the mentor dashboard page'
+    step 'I should be on the admin dashboard page'
   end
 end
--- features/support/paths.rb
+++ features/support/paths.rb
@@ -48,6 +48,8 @@ module NavigationHelpers
       # chat_path
     when /^the donation page$/
       new_donate_path
+    when /^the [u|U]pload page$/
+      resources_path
     else
       begin
         page_name =~ /^the (.*) page$/
