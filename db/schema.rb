fd9fda4ad8b0318a4ee2bfa7a0776589e930b16f
Bao Mai <baomai@berkeley.edu>
2016-11-15 21:37:15 -0800

    Added content column to message model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161112182454) do
+ActiveRecord::Schema.define(version: 20161116053651) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 20161112182454) do
     t.boolean  "read"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.text     "content"
     t.index ["conversation_id"], name: "index_messages_on_conversation_id"
     t.index ["mentor_id"], name: "index_messages_on_mentor_id"
     t.index ["student_id"], name: "index_messages_on_student_id"
@@ -93,6 +94,11 @@ ActiveRecord::Schema.define(version: 20161112182454) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_students_on_email", unique: true
     t.index ["reset_password_token"], name: "index_students_on_reset_password_token", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "writings", force: :cascade do |t|
