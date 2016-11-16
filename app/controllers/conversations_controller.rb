3bd62a35607bad5fc448ac217868ba0eefd54447
Bao Mai <baomai@berkeley.edu>
2016-11-15 21:34:13 -0800

    some important changes

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -15,15 +15,22 @@ class ConversationsController < ApplicationController
 
   def create
     @conversation = Conversation.new
+
+    # create associations
     @conversation.create_student(id: current_user.id)
     @conversation.create_mentor(id: Mentor.first.id) # will replace with active mentor later
-
     @conversation.save!
+
+    # reloads the page
+    redirect_to :back
   end
 
   def show
     @conversation = Conversation.find_by(slug: params[:slug])
     @message = Message.new
+  end
+
+  def edit
   end
 
 end
