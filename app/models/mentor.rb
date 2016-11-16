74593fc3d75182143cf4652ae9de0024e11c5177
Bao Mai <baomai@berkeley.edu>
2016-11-15 22:19:52 -0800

    before migration

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -11,4 +11,9 @@ class Mentor < ApplicationRecord
   has_many :conversations, dependent: :destroy
   has_many :messages, as: :student_or_mentor
 
+  # for use with distinguishing user type
+  def type
+    :mentor
+  end
+
 end
