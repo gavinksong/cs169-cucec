af402f638dc579bc7697b0d35730c4e1f58c7000
Bao Mai <baomai@berkeley.edu>
2016-11-15 18:04:30 -0800

    Auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  # use for current_mentor and current_student
+  include Devise::Controllers::Helpers
+
   def after_sign_in_path_for(resource)
     if student_signed_in?
       return students_path
@@ -13,7 +17,14 @@ class ApplicationController < ActionController::Base
     #admin redirect to come in here as well
   end
   
+  # def current_user
+  #   @current_user ||= (warden.authenticate(scope: :student) || nil)
+  # end
+
+
+  # for model-agnostic features like chat
   def current_user
-    @current_user ||= (warden.authenticate(scope: :student) || nil)
+    @current_user = current_mentor || current_student
   end
+
 end
