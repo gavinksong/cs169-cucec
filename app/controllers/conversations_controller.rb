321dc241396741bcd245a69a3b8b25765b4bc052
Bao Mai <baomai@berkeley.edu>
2016-11-15 21:21:47 -0800

    Added create conversation and show conversations

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -2,12 +2,23 @@ class ConversationsController < ApplicationController
 
   def index
     # TODO: Find all conversations created by user
-    @conversation = nil
+    if student_signed_in?
+      @conversations = current_user.conversations
+    else
+      flash[:info] = 'Please sign in'
+      redirect_to new_student_session_path
+    end
   end
 
   def new
-    @student = current_user
+  end
+
+  def create
     @conversation = Conversation.new
+    @conversation.create_student(id: current_user.id)
+    @conversation.create_mentor(id: Mentor.first.id) # will replace with active mentor later
+
+    @conversation.save!
   end
 
   def show
--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,6 +1,6 @@
 class Conversation < ApplicationRecord
-  belongs_to :student #, :foreign_key => :student_id, class_name:"Student"
-  belongs_to :mentor  #, :foreign_key => :mentor_id, class_name: "Mentor"
+  belongs_to :student, :foreign_key => :student_id, class_name:"Student"
+  belongs_to :mentor, :foreign_key => :mentor_id, class_name: "Mentor"
 
   has_many  :messages, dependent: :destroy
 
