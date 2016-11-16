9bd217f4334295dc9b4926e3ffbe179ef4bfc912
Bao Mai <baomai@berkeley.edu>
2016-11-15 19:08:09 -0800

    Added factoryGirl configurations

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,8 @@
+module SessionsHelper
+  include Devise::Controllers::Helpers
+
+  # for model-agnostic features like chat
+  def current_user
+    @current_user = current_mentor || current_student
+  end
+end
