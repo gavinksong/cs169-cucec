5ecf23d31e5ab34b2ead8b88a7cf6bff7cb1f3eb
gavinksong <gavinksong@gmail.com>
2016-11-17 04:32:20 +0000

    Production env configs for ActionCable.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.web_socket_server_url = "wss://cs169-cucec.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://cs169-cucec.herokuapp.com']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
