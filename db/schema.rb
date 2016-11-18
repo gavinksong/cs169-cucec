837ffa2b99760b780a381a37a4d49f9128adbe31
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-18 15:20:53 -0800

    recreated writings migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161118230820) do
+ActiveRecord::Schema.define(version: 20161118231909) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -108,6 +108,11 @@ ActiveRecord::Schema.define(version: 20161118230820) do
     t.text    "sample"
     t.integer "student_id"
     t.string  "date"
+    t.text     "sample"
+    t.integer  "student_id"
+    t.string   "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
