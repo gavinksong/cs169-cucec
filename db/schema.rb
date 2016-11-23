eb37805a83d624e771cb0596a2a75ad874fdac0f
Emilio Aurea <emilioaurea@gmail.com>
2016-11-23 19:52:05 +0000

    created mentors_available_table, did not inlcude migration file

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161116064625) do
+ActiveRecord::Schema.define(version: 20161123195023) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -56,6 +56,10 @@ ActiveRecord::Schema.define(version: 20161116064625) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_mentors_on_email", unique: true
     t.index ["reset_password_token"], name: "index_mentors_on_reset_password_token", unique: true
+  end
+
+  create_table "mentors_available_tables", force: :cascade do |t|
+    t.string "email"
   end
 
   create_table "messages", force: :cascade do |t|
