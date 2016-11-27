2fde7117941c4c1a530ca0010d64f5cc605739ad
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-27 03:03:02 -0800

    refactored resources feature for watch, listen and text

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -9,32 +9,35 @@ class Resource < ApplicationRecord
     # video uploader
     mount_uploader :video, VideoUploader
     # validates_presence_of :video
-    validates_integrity_of :video #resource upload logic should be refactored to allow thus validation
-    validates_processing_of :video
+    # validates_integrity_of :video #resource upload logic should be refactored to allow thus validation
+    # validates_processing_of :video
     
     # audio uploader
     mount_uploader :audio, AudioUploader
     
     def self.read_english
-        # YYYY-MM-DD_feature_extra
-        file = "#{Rails.public_path}/uploads/#{Date.today.to_s}_read_english.txt"
+        # YYYY-MM-DD_read_english.txt
+        file = "#{Rails.public_path}/text/#{Date.today.to_s}_read_english.txt"
         return Pathname.new(file).exist? ? file : "public/empty.txt"
     end
     
     def self.read_chinese
-        # YYYY-MM-DD_feature_extra
-        file = "#{Rails.public_path}/uploads/#{Date.today.to_s}_read_chinese.txt"
+        # YYYY-MM-DD_read_chinese.txt
+        file = "#{Rails.public_path}/text/#{Date.today.to_s}_read_chinese.txt"
         return Pathname.new(file).exist? ? file : "public/empty.txt"
     end
     
     def self.listen
-        file_path = "public/audios/#{Date.today.to_s}_listen.mp3"
+        # YYYY-MM-DD_listen.mp3
+        file_path = "#{Rails.public_path}/audios/#{Date.today.to_s}_listen.mp3"
         file_name = "#{Date.today.to_s}_listen.mp3"
-        return Pathname.new(file_path).exist? ? file_name : "#"
+        return Pathname.new(file_path).exist? ? file_name : ""
     end
     
-    def self.retrieve_video
-      resource = Resource.order("created_at").last #finds newest resource
-      return resource
+    def self.watch
+      # YYYY-MM-DD_watch.mp4
+      file_path = "#{Rails.public_path}/videos/#{Date.today.to_s}_watch.mp4"
+      file_name = "#{Date.today.to_s}_watch.mp4"
+      return Pathname.new(file_path).exist? ? file_name : ""
     end
 end
