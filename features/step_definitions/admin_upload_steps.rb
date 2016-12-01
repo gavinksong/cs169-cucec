17d4fbf1a951f977f9f6cb649fcdec0c580f90dd
Samuel Karani Mbaabu <samuel.karani@berkeley.edu>
2016-11-30 23:21:56 -0800

    added audio cukes

--- features/step_definitions/admin_upload_steps.rb
+++ features/step_definitions/admin_upload_steps.rb
@@ -1,6 +1,6 @@
 require 'pathname'
 
-When(/^I upload "([^"]*)" for "([^"]*)" for (today|tomorrow)$/) do |type, feature, day|
+When(/^I upload text "([^"]*)" for "([^"]*)" for (today|tomorrow)$/) do |type, feature, day|
   click_link "new_resource"
   if day == "today"
     day = Date.today.to_s
@@ -12,6 +12,18 @@ When(/^I upload "([^"]*)" for "([^"]*)" for (today|tomorrow)$/) do |type, featur
   click_button 'Save Text'
 end
 
+When(/^I upload audio "([^"]*)" for "([^"]*)" for (today|tomorrow)$/) do |type, feature, day|
+  click_link "new_resource"
+  if day == "today"
+    day = Date.today.to_s
+  else
+    day = (Date.today + 1).to_s
+  end
+  path = "public/#{day}_" + feature + "." + type
+  page.attach_file("resource_audio", path)
+  click_button 'Save Audio'
+end
+
 Given(/^it is tomorrow$/) do
   class Date
     @tom = self.today + 1
@@ -21,7 +33,7 @@ Given(/^it is tomorrow$/) do
   end
 end
 
-Given(/^no content has been uploaded$/) do
+Given(/^no text content has been uploaded$/) do
   if Pathname.new('public/text').directory?
     Dir.foreach('public/text') do |item|
       next if item == '.' or item == '..' or item == 'tmp'
@@ -30,8 +42,26 @@ Given(/^no content has been uploaded$/) do
   end
 end
 
+Given(/^no audio content has been uploaded$/) do
+  if Pathname.new('public/audios').directory?
+    Dir.foreach('public/audios') do |item|
+      next if item == '.' or item == '..' or item == 'tmp'
+      FileUtils.rm_rf('public/audios/' + item)
+    end
+  end
+end
+
+Given(/^no video content has been uploaded$/) do
+  if Pathname.new('public/videos').directory?
+    Dir.foreach('public/videos') do |item|
+      next if item == '.' or item == '..' or item == 'tmp'
+      FileUtils.rm_rf('public/videos/' + item)
+    end
+  end
+end
+
 Then (/^Delete all files$/) do
-  step "no content has been uploaded"
+  step "I haven't uploaded any files to the app"
   Dir.foreach('public') do |item|
     if item.start_with?((Date.today - 1).to_s, (Date.today).to_s, (Date.today + 1).to_s) # lol messed up date class
       FileUtils.rm_rf('public/' + item)
@@ -39,21 +69,38 @@ Then (/^Delete all files$/) do
   end
 end
 
-Given /^I successfully upload files for (.*)$/ do |day|
+Given /^I successfully upload text files for (.*)$/ do |day|
   steps %{
     Then I should be on the admin dashboard page
     When I follow "Upload"
     Then I should be on the Upload page
-    When I upload "txt" for "read_english" for #{day}
+    When I upload text "txt" for "read_english" for #{day}
     Then I should see "Upload successful"
     Then I should be on the Upload page
-    When I upload "txt" for "read_chinese" for #{day}
+    When I upload text "txt" for "read_chinese" for #{day}
+    Then I should see "Upload successful"
+  }
+end
+
+
+Given /^I successfully upload audio files for (.*)$/ do |day|
+  steps %{
+    Then I should be on the admin dashboard page
+    When I follow "Upload"
+    Then I should be on the Upload page
+    When I upload audio "mp3" for "listen" for #{day}
     Then I should see "Upload successful"
   }
 end
 
 Given(/^I haven't uploaded any files to the app$/) do
-  step "no content has been uploaded"
+  step "no text content has been uploaded"
+  step "no audio content has been uploaded"
+  step "no video content has been uploaded"
+end
+
+Then(/^I should see an audio file$/) do
+  page.should have_css "#audio_available"
 end
 
 Given(/^the following files exist on my local computer with (today|tomorrow)'s date:$/) do |day, table|
